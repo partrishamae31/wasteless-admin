3d44dc4c54cceb27f1f45d821f9da05756aea8c1
partrishamae31 <partrishamae31@gmail.com>
2026-04-17 21:28:06 +0800

    Remove node_modules and add gitignore

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 // Replace these with your actual Supabase URL and Anon Key
 // You can find these in your Supabase Dashboard > Project Settings > API
 const supabaseUrl = 'https://hpwwojprqyrfzruuvwhk.supabase.co' 
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY_HERE'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhwd3dvanBycXlyZnpydXV2d2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzE4NDMsImV4cCI6MjA4NzQwNzg0M30.DnusMBYWOvOlZNDPTEhzDR9KUCfIQ-J78c4uJ6loVN4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
